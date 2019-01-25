c6d13913ad53d7cdebb55e7ab5b2c4d14ed15cb4
Tony Boyles <Anthony@Boyles.cc>
2019-01-25 14:26:57 -0500

    Tweaked the node demo file

--- demo/fornode.js
+++ demo/fornode.js
@@ -1,10 +1,12 @@
-var patristic = require('../index');
-var D = [
+const patristic = require('../dist/patristic');
+
+const matrix = [
     [0,  5,  9,  9, 8],
     [5,  0, 10, 10, 9],
     [9, 10,  0,  8, 7],
     [9, 10,  8,  0, 3],
     [8,  9,  7,  3, 0]
 ];
-var labels = ['A', 'B', 'C', 'D', 'E'];
-var tree = patristic.parseMatrix(D, labels);
+const labels = ['A', 'B', 'C', 'D', 'E'];
+
+let tree = patristic.parseMatrix(matrix, labels);
