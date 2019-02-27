5e25cf0376bb72b947ab670da87d73e4d976c1ee
Tony Boyles <Anthony@Boyles.cc>
2019-02-27 15:41:00 -0500

    Version bump

--- dist/patristic.js
+++ dist/patristic.js
@@ -11,7 +11,7 @@
    * @example
    * console.log(patristic.version);
    */
-  const version = "0.3.3";
+  const version = "0.3.5";
 
   /**
    * A class for representing Branches in trees.
--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@
  * @example
  * console.log(patristic.version);
  */
-export const version = "0.3.3";
+export const version = "0.3.5";
 
 /**
  * A class for representing Branches in trees.
