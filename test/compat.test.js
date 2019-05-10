d113e8e1ca95a36a8244e199cc6f2e29a62dc17d
Tony Boyles <Anthony@Boyles.cc>
2019-05-10 16:20:46 -0400

    Fixed ancestor test

--- test/compat.test.js
+++ test/compat.test.js
@@ -5,5 +5,5 @@ const newick = "(A:0.1,B:0.2,(C:0.3,D:0.4):0.5);";
 var tree = patristic.parseNewick(newick);
 
 test('Tree can return ancestors like d3-hierarchy', () => {
-  expect(tree.children[2].children[1].ancestors()).toEqual([tree, tree.children[2], tree.children[2].children[1]]);
+  expect(tree.children[2].children[1].ancestors()).toEqual([tree.children[2].children[1], tree.children[2], tree]);
 });
