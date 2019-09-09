30e343b104e7cb879d004b3cfd51a4758598494b
Tony Boyles <nsp3@cdc.gov>
2019-09-09 14:15:29 -0400

    simplified guid code

--- src/main.js
+++ src/main.js
@@ -35,12 +35,10 @@ export function Branch(data, children) {
   });
 }
 
-function guid(a) {
-  if (a) {
+function guid(){
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, a => {
     return (a ^ ((Math.random() * 16) >> (a / 4))).toString(16);
-  } else {
-    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, guid);
-  }
+  });
 }
 
 /**
